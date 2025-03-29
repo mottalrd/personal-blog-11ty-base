49f6bb858949e4ab6f0f6fbbf7dc7ca7e10ccf57
Alfredo Motta <motta.lrd@hotmail.it>
2025-03-29 20:27:26 +0100

    change title and email

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Alfredo Motta Personal Blog",
+	title: "Alfredo Motta",
 	url: "https://www.alfredomotta.me/",
 	language: "en",
-	description: "Personal Website of Alfredo Motta.",
+	description: "CTO in Technology and Financial Services",
 	author: {
 		name: "Alfredo Motta",
-		email: "alfredo_blog@mailinator.com",
+		email: "last.blade6351@fastmail.com",
 		url: "https://www.alfredomotta.me/about-me/"
 	}
 }
