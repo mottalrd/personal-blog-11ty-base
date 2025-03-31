03b98c95fdfab7e61161e2b8536959a04afeb5aa
Alfredo Motta <motta.lrd@hotmail.it>
2025-03-31 15:20:34 +0200

    Remove XML feed until blog is not present

--- eleventy.config.js
+++ eleventy.config.js
@@ -51,12 +51,6 @@ export default async function(eleventyConfig) {
 		type: "atom", // or "rss", "json"
 		outputPath: "/feed/feed.xml",
 		stylesheet: "pretty-atom-feed.xsl",
-		templateData: {
-			eleventyNavigation: {
-				key: "Feed",
-				order: 10
-			}
-		},
 		collection: {
 			name: "posts",
 			limit: 10,
